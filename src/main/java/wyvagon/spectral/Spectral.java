667ce61e6f502cda119fb07c2934ef71e8831283
wyvagon <wyvagonMC@gmail.com>
2019-02-28 18:51:17 -0800

    Initial Forge setup

---
+++ src/main/java/wyvagon/spectral/Spectral.java
@@ -0,0 +1,42 @@
+package wyvagon.spectral;
+
+import wyvagon.spectral.proxy.CommonProxy;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = Spectral.MODID, name = Spectral.NAME, version = Spectral.VERSION, useMetadata = true)
+public class Spectral {
+
+	public static final String MODID = "spectral";
+	public static final String NAME = "Spectral";
+	public static final String VERSION = "1.0.0";
+
+	@SidedProxy(clientSide = "wyvagon.spectral.proxy.ClientProxy", serverSide = "wyvagon.spectral.proxy.CommonProxy")
+	public static CommonProxy proxy;
+
+	@Mod.Instance
+	public static Spectral instance;
+
+	public static Logger logger;
+
+	@Mod.EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		logger = event.getModLog();
+		proxy.preInit(event);
+	}
+
+	@Mod.EventHandler
+	public void init(FMLInitializationEvent event) {
+		proxy.init(event);
+	}
+
+	@Mod.EventHandler
+	public void postInit(FMLPostInitializationEvent event) {
+		proxy.postInit(event);
+	}
+}
