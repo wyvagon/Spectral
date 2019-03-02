0eda0d37618c0479787e08198319e566c352cad5
wyvagon <wyvagonMC@gmail.com>
2019-03-01 17:01:44 -0800

    Added first item along with creative tab and US localization file

--- src/main/java/wyvagon/spectral/Spectral.java
+++ src/main/java/wyvagon/spectral/Spectral.java
@@ -1,6 +1,10 @@
 package wyvagon.spectral;
 
+import wyvagon.spectral.core.ItemHandler;
 import wyvagon.spectral.proxy.CommonProxy;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -15,6 +19,12 @@ public class Spectral {
 	public static final String MODID = "spectral";
 	public static final String NAME = "Spectral";
 	public static final String VERSION = "1.0.0";
+	public static final CreativeTabs SPECTRAL_TAB = new CreativeTabs(MODID) {
+		@Override
+		public ItemStack getTabIconItem() {
+			return new ItemStack(ItemHandler.SOUL_JAR);
+		}
+	};
 
 	@SidedProxy(clientSide = "wyvagon.spectral.proxy.ClientProxy", serverSide = "wyvagon.spectral.proxy.CommonProxy")
 	public static CommonProxy proxy;
@@ -39,4 +49,5 @@ public class Spectral {
 	public void postInit(FMLPostInitializationEvent event) {
 		proxy.postInit(event);
 	}
+
 }
--- src/main/java/wyvagon/spectral/core/ItemHandler.java
+++ src/main/java/wyvagon/spectral/core/ItemHandler.java
@@ -9,18 +9,27 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
+import java.util.ArrayList;
 import java.util.List;
 
 //Holds references to items and registers items on Item RegistryEvent
 @Mod.EventBusSubscriber
 @GameRegistry.ObjectHolder(Spectral.MODID)
+@SuppressWarnings("unchecked")
 public class ItemHandler {
+
+	public static final Item SOUL_JAR = null;
 	
 	public static List<Item> items;
 
 	@SubscribeEvent
 	public static void registerItems(RegistryEvent.Register<Item> event) {
 		items = new ArrayList();
+		items.add(
+			new ItemSoulJar().setRegistryName("soul_jar")
+			);
+
+		event.getRegistry().registerAll(items.toArray(new Item[0]));
 	}
 
 
